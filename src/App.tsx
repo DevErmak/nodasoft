dac46c6139d058bcfa891c27d30628ab728d1f46
DevErmak <o.khasanov.dev@gmail.com>
2024-04-04 16:39:39 +0300

    savedata

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { memo, useCallback, useEffect, useRef, useState } from "react";
+import "./App.css";
+const URL = "https://jsonplaceholder.typicode.com/users";
+
+type Company = {
+  bs: string;
+  catchPhrase: string;
+  name: string;
+};
+
+type User = {
+  id: number;
+  email: string;
+  name: string;
+  phone: string;
+  username: string;
+  website: string;
+  company: Company;
+  address: any;
+};
+
+interface IButtonProps {
+  onClick: any;
+}
+
+function Button({ onClick }: IButtonProps): JSX.Element {
+  return (
+    <button type="button" onClick={onClick}>
+      get random user
+    </button>
+  );
+}
+
+interface IUserInfoProps {
+  user: User;
+}
+
+function UserInfo({ user }: IUserInfoProps): JSX.Element {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Username</th>
+          <th>Phone number</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{user.name}</td>
+          <td>{user.phone}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+const UserInfoMemo = memo(UserInfo);
+
+function ErrorInfo(): JSX.Element {
+  return <div>No data</div>;
+}
+
+function useThrottle<T extends any[]>(fn: (...args: T) => any, wait = 0) {
+  const timer = useRef<ReturnType<typeof setTimeout>>();
+  const fnRef = useRef(fn);
+  const currentArgs = useRef<T>();
+
+  fnRef.current = fn;
+
+  const callback = useCallback(
+    (...args: T) => {
+      currentArgs.current = args;
+
+      if (!timer.current) {
+        timer.current = setTimeout(() => {
+          fnRef.current(...(currentArgs.current as T));
+          timer.current = undefined;
+        }, wait);
+      }
+    },
+    [wait]
+  );
+
+  return callback;
+}
 
 function App() {
+  const [item, setItem] = useState<User>();
+  const [isError, setIsError] = useState<boolean>(false);
+
+  const receiveRandomUser = async () => {
+    const id = Math.floor(Math.random() * (10 - 1)) + 1;
+    try {
+      setIsError(false);
+      const response = await fetch(`${URL}/${id}`);
+      const _user = (await response.json()) as User;
+      setItem(_user);
+    } catch (err) {
+      setIsError(true);
+      setItem(undefined);
+    }
+  };
+
+  const receiveRandomUserThrottle = useThrottle(receiveRandomUser, 1000);
+
+  const handleButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.stopPropagation();
+    receiveRandomUserThrottle();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>Get a random user</header>
+      <Button onClick={handleButtonClick} />
+      {item && <UserInfoMemo user={item} />}
+      {isError && <ErrorInfo />}
     </div>
   );
 }
